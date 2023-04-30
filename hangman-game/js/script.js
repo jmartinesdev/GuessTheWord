30866b1a8ec1ee4b2709eca8bddf082ff5293525
Jakeline Martines <jakeline_13_martines@hotmail.com>
2023-04-30 20:15:34 +0100

    error fixed
    some commas were missing it

--- hangman-game/js/script.js
+++ hangman-game/js/script.js
@@ -1,193 +1,202 @@
-let words = [
-word01 = {
-    letter: 'france',
-    categories: 'countries'
+const words = [
+    word001 = {
+        letter: 'france',
+        categories: 'countries'
     },
-word02 = {
-    letter: 'germany',
-    categories:  'countries'
+    word002 = {
+        letter: 'germany',
+        categories:  'countries'
     },
-word03 = {
-    letter: 'england',
-    categories: 'countries'
+
+    word003 = {
+        letter: 'england',
+        categories: 'countries'
+    },
+        
+    word004 = {
+        letter: 'india',
+        categories: 'countries'
+    },    
+
+    word005 = {
+        letter: 'portugal',
+        categories: 'countries'
+    },
+
+    word006 = {
+        letter: 'spain',
+        categories: 'countries',
+    },
+
+    word007 = {
+        letter: 'brazil',
+        categories: 'countries'
+    },
+
+    word008 = {
+        letter: 'argentina',
+        categories: 'countries'
+    },
+
+    word009 = {
+        letter: 'chile',
+        categories: 'countries'
+    },
+
+    word010 = {
+        letter: 'canada',
+        categories: 'countries'
+    },
+
+    word011 = {
+        letter: 'sausages',
+        categories: 'food'
+    },
+
+    word012 = {
+        letter: 'cookie',
+        categories: 'food'
+    },
+
+    word013 = {
+        letter: 'melon',
+        categories: 'food'
+    },
+
+    word014 = {
+        letter: 'cupcake',
+        categories: 'food'
+    },
+
+    word015 = {
+        letter: 'cabbage',
+        categories: 'food'
+    },
+
+    word016 = {
+        letter: 'shrimp',
+        categories: 'food'
+    },
+
+    word017 = {
+        letter: 'porridge',
+        categories: 'food'
+    },
+
+    word018 = {
+        letter: 'pancake',
+        categories: 'food'
+    },
+
+    word019 = {
+        letter: 'hamburger',
+        categories: 'food'
+    },
+
+    word020 = {
+        letter: 'raspberries',
+        categories: 'food'
+    },
+
+    word021 = {
+        letter: 'samsung',
+        categories: 'brands'
+    },
+
+    word022 = {
+        letter: 'apple',
+        categories: 'brands'
+    },
+
+    word023 = {
+        letter: 'coca-cola',
+        categories: 'brands'
+    },
+
+    word024 = {
+        letter: 'vodafone',
+        categories: 'brands'
+    },
+
+    word025 = {
+        letter: 'tesco',
+        categories: 'brands'
+    },
+
+    word026 = {
+        letter: 'youtube',
+        categories: 'brands'
+    },
+
+    word027 = {
+        letter: 'playstation',
+        categories: 'brands'
+    },
+
+    word028 = {
+        letter: 'lenovo',
+        categories: 'brands'
+    },
+
+    word029 = {
+        letter: 'hyundai',
+        categories: 'brands'
+    },
+
+    word030 = {
+        letter: 'adidas',
+        categories: 'brands'
+    },
+
+    word031 = {
+        letter: 'friends',
+        categories: 'tvShow'
+    },
+
+    word032 = {
+        letter: 'greys anatomy',
+        categories: 'tvShow'
+    },
+
+    word033 = {
+        letter: 'suits',
+        categories: 'tvShow'
+    },
+
+    word034 = {
+        letter: 'outlander',
+        categories: 'tvShow'
+    },
+
+    word035 = {
+        letter: 'psych',
+        categories: 'tvShow'
+    },
+
+    word036 = {
+        letter: 'you',
+        categories: 'tvShow'
+    },
+
+    word037 = {
+        letter: 'fleabag',
+        categories: 'tvShow'
+    },
+
+    word038 = {
+        letter: 'hannibal',
+        categories: 'tvShow'
+    },
+
+    word039 = {
+        letter: 'superGirl',
+        categories: 'tvShow'
+    },
+
+    word040 = {
+        letter: 'twilight',
+        categories: 'tvShow'
     }
-    
-word04 = {
-    letter: 'france',
-    categories: 'countries'
-},
-
-word05 = {
-    letter: 'portugal',
-    categories: 'countries'
-},
-
-word06 = {
-    letter: 'spain',
-    categories: 'countries',
-    },
-word07 = {
-    letter: 'brazil',
-    categories: 'countries'
-    },
-word08 = {
-    letter: 'argentina',
-    categories: 'countries'
-    },
-word09 = {
-    letter: 'chile',
-    categories: 'countries'
-    },
-word10 = {
-    letter: 'canada',
-    categories: 'countries'
-    },
-word11 = {
-    letter: 'sausages',
-    categories: 'food'
-    },
-word12 = {
-    letter: 'cookie',
-    categories: 'food'
-    },
-word13 = {
-    letter: 'melon',
-    categories: 'food'
-    },
-word14 = {
-    letter: 'cupcake',
-    categories: 'food'
-},
-
-word15 = {
-    letter: 'cabbage',
-    categories: 'food'
-},
-
-word16 = {
-    letter: 'shrimp',
-    categories: 'food'
-},
-
-word17 = {
-    letter: 'porridge',
-    categories: 'food'
-},
-
-word18 = {
-    letter: 'pancake',
-    categories: 'food'
-},
-
-word19 = {
-    letter: 'hamburger',
-    categories: 'food'
-},
-
-word20 = {
-    letter: 'raspberries',
-    categories: 'food'
-},
-
-word21 = {
-    letter: 'samsung',
-    categories: 'brands'
-},
-
-word22 = {
-    letter: 'apple',
-    categories: 'brands'
-},
-
-word23 = {
-    letter: 'coca-cola',
-    categories: 'brands'
-},
-
-word24 = {
-    letter: 'vodafone',
-    categories: 'brands'
-},
-
-word25 = {
-    letter: 'tesco',
-    categories: 'brands'
-},
-
-word26 = {
-    letter: 'youtube',
-    categories: 'brands'
-},
-
-word27 = {
-    letter: 'playstation',
-    categories: 'brands'
-},
-
-word28 = {
-    letter: 'lenovo',
-    categories: 'brands'
-},
-
-word29 = {
-    letter: 'hyundai',
-    categories: 'brands'
-},
-
-word30 = {
-    letter: 'adidas',
-    categories: 'brands'
-}
-
-word31 = {
-    letter: 'friends',
-    categories: 'tvShow'
-},
-
-word32 = {
-    letter: 'greys anatomy',
-    categories: 'tvShow'
-},
-
-word33 = {
-    letter: 'suits',
-    categories: 'tvShow'
-},
-
-word34 = {
-    letter: 'outlander',
-    categories: 'tvShow'
-},
-
-word35 = {
-    letter: 'psych',
-    categories: 'tvShow'
-}
-
-word36 = {
-    letter: 'you',
-    categories: 'tvShow'
-},
-
-word37 = {
-    letter: 'fleabag',
-    categories: 'tvShow'
-},
-
-word38 = {
-    letter: 'hannibal',
-    categories: 'tvShow'
-},
-
-word39 = {
-    letter: 'superGirl',
-    categories: 'tvShow'
-},
-
-word40 = {
-    letter: 'twilight',
-    categories: 'tvShow'
-}
 ];
 
 findTheWord()
