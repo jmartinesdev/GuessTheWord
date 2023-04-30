0fbee099c84047384c3b9aec491e92e3e1524787
Jakeline Martines <jakeline_13_martines@hotmail.com>
2023-04-30 19:12:09 +0100

    fixed an error
    ntaxError: Unexpected identifier 'word04' (a
    
    replace = for :

--- hangman-game/js/script.js
+++ hangman-game/js/script.js
@@ -1,194 +1,196 @@
 let words = [
 word01 = {
-    letter = 'france',
-    categories = 'countries'
+    letter: 'france',
+    categories: 'countries'
     },
 word02 = {
-    letter = 'germany',
-    categories = 'countries'
+    letter: 'germany',
+    categories:  'countries'
     },
 word03 = {
-    letter = 'england',
-    categories = 'countries'
+    letter: 'england',
+    categories: 'countries'
     }
     
 word04 = {
-    letter = 'france',
-    categories = 'countries'
+    letter: 'france',
+    categories: 'countries'
 },
 
 word05 = {
-    letter = 'portugal',
-    categories = 'countries'
-    },
+    letter: 'portugal',
+    categories: 'countries'
+},
+
 word06 = {
-    letter = 'spain',
-    categories = 'countries',
+    letter: 'spain',
+    categories: 'countries',
     },
 word07 = {
-    letter = 'brazil',
-    categories = 'countries'
+    letter: 'brazil',
+    categories: 'countries'
     },
 word08 = {
-    letter = 'argentina',
-    categories = 'countries'
+    letter: 'argentina',
+    categories: 'countries'
     },
 word09 = {
-    letter = 'chile',
-    categories = 'countries'
+    letter: 'chile',
+    categories: 'countries'
     },
 word10 = {
-    letter = 'canada',
-    categories = 'countries'
+    letter: 'canada',
+    categories: 'countries'
     },
 word11 = {
-    letter = 'sausages',
-    categories = 'food'
+    letter: 'sausages',
+    categories: 'food'
     },
 word12 = {
-    letter = 'cookie',
-    categories = 'food'
+    letter: 'cookie',
+    categories: 'food'
     },
 word13 = {
-    letter = 'melon',
-    categories = 'food'
+    letter: 'melon',
+    categories: 'food'
     },
 word14 = {
-    letter = 'cupcake',
-    categories = 'food'
+    letter: 'cupcake',
+    categories: 'food'
 },
 
 word15 = {
-    letter = 'cabbage',
-    categories = 'food'
+    letter: 'cabbage',
+    categories: 'food'
 },
 
 word16 = {
-    letter = 'shrimp',
-    categories = 'food'
+    letter: 'shrimp',
+    categories: 'food'
 },
 
 word17 = {
-    letter = 'porridge',
-    categories = 'food'
+    letter: 'porridge',
+    categories: 'food'
 },
 
 word18 = {
-    letter = 'pancake',
-    categories = 'food'
+    letter: 'pancake',
+    categories: 'food'
 },
 
 word19 = {
-    letter = 'hamburger',
-    categories = 'food'
+    letter: 'hamburger',
+    categories: 'food'
 },
 
 word20 = {
-    letter = 'raspberries',
-    categories = 'food'
+    letter: 'raspberries',
+    categories: 'food'
 },
 
 word21 = {
-    letter = 'samsung',
-    categories = 'brands'
+    letter: 'samsung',
+    categories: 'brands'
 },
 
 word22 = {
-    letter = 'apple',
-    categories = 'brands'
+    letter: 'apple',
+    categories: 'brands'
 },
 
 word23 = {
-    letter = 'coca-cola',
-    categories = 'brands'
+    letter: 'coca-cola',
+    categories: 'brands'
 },
 
 word24 = {
-    letter = 'vodafone',
-    categories = 'brands'
+    letter: 'vodafone',
+    categories: 'brands'
 },
 
 word25 = {
-    letter = 'tesco',
-    categories = 'brands'
+    letter: 'tesco',
+    categories: 'brands'
 },
 
 word26 = {
-    letter = 'youtube',
-    categories = 'brands'
+    letter: 'youtube',
+    categories: 'brands'
 },
 
 word27 = {
-    letter = 'playstation',
-    categories = 'brands'
+    letter: 'playstation',
+    categories: 'brands'
 },
 
 word28 = {
-    letter = 'lenovo',
-    categories = 'brands'
+    letter: 'lenovo',
+    categories: 'brands'
 },
 
 word29 = {
-    letter = 'hyundai',
-    categories = 'brands'
+    letter: 'hyundai',
+    categories: 'brands'
 },
 
 word30 = {
-    letter = 'adidas',
-    categories = 'brands'
+    letter: 'adidas',
+    categories: 'brands'
 }
 
 word31 = {
-    letter = 'friends',
-    categories = 'tvShow'
+    letter: 'friends',
+    categories: 'tvShow'
 },
 
 word32 = {
-    letter = 'greys anatomy',
-    categories = 'tvShow'
+    letter: 'greys anatomy',
+    categories: 'tvShow'
 },
 
 word33 = {
-    letter = 'suits',
-    categories = 'tvShow'
+    letter: 'suits',
+    categories: 'tvShow'
 },
 
 word34 = {
-    letter = 'outlander',
-    categories = 'tvShow'
+    letter: 'outlander',
+    categories: 'tvShow'
 },
 
 word35 = {
-    letter = 'psych',
-    categories = 'tvShow'
+    letter: 'psych',
+    categories: 'tvShow'
 }
 
 word36 = {
-    letter = 'you',
-    categories = 'tvShow'
+    letter: 'you',
+    categories: 'tvShow'
 },
 
 word37 = {
-    letter = 'fleabag',
-    categories = 'tvShow'
+    letter: 'fleabag',
+    categories: 'tvShow'
 },
 
 word38 = {
-    letter = 'hannibal',
-    categories = 'tvShow'
+    letter: 'hannibal',
+    categories: 'tvShow'
 },
 
 word39 = {
-    letter = 'superGirl',
-    categories = 'tvShow'
+    letter: 'superGirl',
+    categories: 'tvShow'
 },
 
 word40 = {
-    letter = 'twilight',
-    categories = 'tvShow'
+    letter: 'twilight',
+    categories: 'tvShow'
 }
 ];
 
+findTheWord()
 function findTheWord(){
     let indexWord = parseInt(Math.random() * words.length)
     console.log(indexWord)
